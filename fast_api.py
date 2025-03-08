93d6e96e217921564f849f280a31ee2969f3a64b
Nishan-k <karki.nishann@gmail.com>
2025-03-08 16:12:58 +0100

    changes

--- fast_api.py
+++ fast_api.py
@@ -30,5 +30,5 @@ def prediction(input_features:Input_features):
     return {"prediction": result.tolist()}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Render assigns a PORT
+    port = int(os.getenv("PORT", 8000)) 
     uvicorn.run(app, host="0.0.0.0", port=port)
