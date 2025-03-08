59270b735dee5f855451bc79d36adbcd6cec583f
Nishan-k <karki.nishann@gmail.com>
2025-03-08 13:47:44 +0100

    changes

--- fast_api.py
+++ fast_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import joblib
 from pydantic import BaseModel
 import pandas as pd
+import uvicorn
 
 # Load the  model:
 penguin_clf = joblib.load("./notebook/penguin_clf.pkl")
@@ -27,3 +28,7 @@ def prediction(input_features:Input_features):
     input_data = pd.DataFrame([input_features.model_dump()])
     result = penguin_clf.predict(input_data)
     return {"prediction": result.tolist()}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Render assigns a PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
